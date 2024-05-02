0af5b19bd1a6b81f74774aa9e43e0cce1c044420
hiromu-t396 <hiromu1193966@gmail.com>
2024-05-02 22:49:19 +0900

    storage accountの作成と静的ファイルの配置

--- storage.tf
+++ storage.tf
@@ -4,4 +4,26 @@ resource "azurerm_storage_account" "storage" {
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
+  static_website {
+    index_document = "index.html"
+    error_404_document = "error.html"
+  }
+}
+
+resource "azurerm_storage_blob" "html" {
+  name                   = "index.html"
+  storage_account_name   = azurerm_storage_account.storage.name
+  storage_container_name = "$web"
+  type                   = "Block"
+  content_type           = "text/html"
+  source                 = "index.html"
+}
+
+resource "azurerm_storage_blob" "css" {
+  name                   = "style.css"
+  storage_account_name   = azurerm_storage_account.storage.name
+  storage_container_name = "$web"
+  type                   = "Block"
+  content_type           = "text/css"
+  source                 = "style.css"
 }
