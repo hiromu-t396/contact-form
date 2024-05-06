9f4a33468c1011c1e3dc009c2242df3ec27c95a9
hiromu-t396 <hiromu1193966@gmail.com>
2024-05-06 16:57:06 +0900

    DNSゾーンの作成とCNAMEレコードを追加(サブドメインを指定)

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,11 @@
 output "resource_group_name" {
   value = azurerm_resource_group.rg.name
 }
+
+output "storage_account_name" {
+  value = azurerm_storage_account.storage.name
+}
+
+output "primary_web_endpoint" {
+  value = azurerm_storage_account.storage.primary_web_host
+}
