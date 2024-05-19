2a6cca182a930c42d6f3b5559329dbc823d09d64
hiromu-t396 <hiromu1193966@gmail.com>
2024-05-19 12:00:59 +0900

    POSTデータ格納用テーブルストレージを作成

--- storage.tf
+++ storage.tf
@@ -1,33 +1,44 @@
-resource "azurerm_storage_account" "storage" {
-  name                     = "forstaticfile"
+# 静的ファイル保管用
+resource "azurerm_storage_account" "storage_account" {
+  name                     = "forpoc"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
-  static_website {
-    index_document = "index.html"
-    error_404_document = "error.html"
-  }
-   custom_domain {
-    name         = "www.forpoc.com"  # ここにカスタムドメイン名を指定します
-    use_subdomain = false          # サブドメインを使用する場合は true を指定します
-  }
 }
-
+resource "azurerm_storage_container" "storage_container" {
+  name                  = "web"
+  storage_account_name  = azurerm_storage_account.storage_account.name
+  container_access_type = "blob"
+}
 resource "azurerm_storage_blob" "html" {
   name                   = "index.html"
-  storage_account_name   = azurerm_storage_account.storage.name
-  storage_container_name = "$web"
+  storage_account_name   = azurerm_storage_account.storage_account.name
+  storage_container_name = azurerm_storage_container.storage_container.name
   type                   = "Block"
   content_type           = "text/html"
   source                 = "index.html"
 }
-
 resource "azurerm_storage_blob" "css" {
   name                   = "style.css"
-  storage_account_name   = azurerm_storage_account.storage.name
-  storage_container_name = "$web"
+  storage_account_name   = azurerm_storage_account.storage_account.name
+  storage_container_name = azurerm_storage_container.storage_container.name
   type                   = "Block"
   content_type           = "text/css"
   source                 = "style.css"
+}
+
+# Azure Functions用
+resource "azurerm_storage_account" "functions" {
+  name                     = "forpocgroupad9c"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+#POSTデータ格納用 
+resource "azurerm_storage_table" "table" {
+  name                 = "inquiry"
+  storage_account_name = azurerm_storage_account.functions.name
 }
