4387aa21f3493bede6ee0ca2739c26fe9a8f9851
hiromu-t396 <hiromu1193966@gmail.com>
2024-05-28 23:51:32 +0900

    サブドメインの記載を削除

--- dns_zone.tf
+++ dns_zone.tf
@@ -1,13 +1,4 @@
-resource "azurerm_dns_zone" "dns" {
+resource "azurerm_dns_zone" "main" {
   name                = "forpoc.com"
   resource_group_name = azurerm_resource_group.rg.name
-}
-
-# サブドメインの切り出し
-resource "azurerm_dns_cname_record" "cname" {
-  name                = "www"
-  zone_name           = azurerm_dns_zone.dns.name
-  resource_group_name = azurerm_resource_group.rg.name
-  ttl                 = 3600
-  record              = "contact-form-endpoint.azureedge.net"
 }
