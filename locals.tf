1d55ce765cf3caf688b7fa7fb285dff1bbf00d20
hiromu-t396 <hiromu1193966@gmail.com>
2024-05-28 23:52:53 +0900

    変数を追加

--- locals.tf
+++ locals.tf
@@ -1,4 +1,9 @@
 locals {
-  project  = "contact-form"
-  location = "japaneast"
+ project_name = "contact-form"
+ location     = "japaneast"
+ state_file_resource_group_name    = "terraform"
+ state_file_storage_account_name   = "contactformfortfstate"
+ state_file_storage_container_name = "tfstate"
+ root_state_file                   = "terraform.tfstate"
 }
+
